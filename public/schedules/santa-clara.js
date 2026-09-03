1641ef5515763b7116c8ea2bd34ccc761358bec1
xdinorun <162236514+xdinorun@users.noreply.github.com>
2026-09-02 19:35:59 -0700

    Create santa-clara.js

---
+++ public/schedules/santa-clara.js
@@ -0,0 +1,92 @@
+window.PearBellSchedules = window.PearBellSchedules || {};
+
+window.PearBellSchedules.santaClara = {
+    name: "Santa Clara High School",
+    schoolYear: "2026-2027",
+
+    schedules: {
+        monday: [
+            { name: "1st Period", start: "08:45", end: "09:35" },
+            { name: "Passing Period", start: "09:35", end: "09:40" },
+
+            { name: "2nd Period", start: "09:40", end: "10:30" },
+            { name: "Passing Period", start: "10:30", end: "10:35" },
+
+            { name: "3rd Period", start: "10:35", end: "11:25" },
+            { name: "Passing Period", start: "11:25", end: "11:30" },
+
+            { name: "4th Period + Announcements", start: "11:30", end: "12:25" },
+
+            { name: "Lunch", start: "12:25", end: "13:00" },
+            { name: "Passing Period", start: "13:00", end: "13:05" },
+
+            { name: "5th Period", start: "13:05", end: "13:55" },
+            { name: "Passing Period", start: "13:55", end: "14:00" },
+
+            { name: "6th Period", start: "14:00", end: "14:50" },
+            { name: "Passing Period", start: "14:50", end: "14:55" },
+
+            { name: "7th Period", start: "14:55", end: "15:45" }
+        ],
+
+        tuesdayThursday: [
+            { name: "1st Period", start: "08:45", end: "10:15" },
+            { name: "Passing Period", start: "10:15", end: "10:25" },
+
+            { name: "3rd Period + Announcements", start: "10:25", end: "12:00" },
+
+            { name: "Lunch", start: "12:00", end: "12:35" },
+            { name: "Passing Period", start: "12:35", end: "12:40" },
+
+            { name: "5th Period", start: "12:40", end: "14:10" },
+            { name: "Passing Period", start: "14:10", end: "14:20" },
+
+            { name: "7th Period", start: "14:20", end: "15:50" }
+        ],
+
+        wednesdayFriday: [
+            { name: "2nd Period + Announcements", start: "08:45", end: "10:20" },
+            { name: "Passing Period", start: "10:20", end: "10:30" },
+
+            { name: "Tutorial", start: "10:30", end: "11:20" },
+
+            { name: "Lunch", start: "11:20", end: "11:55" },
+            { name: "Passing Period", start: "11:55", end: "12:00" },
+
+            { name: "4th Period", start: "12:00", end: "13:30" },
+            { name: "Passing Period", start: "13:30", end: "13:40" },
+
+            { name: "6th Period", start: "13:40", end: "15:10" }
+        ]
+    },
+
+    specialSchedules: {
+        finalsDay1: [
+            { name: "1st Period Final", start: "08:45", end: "10:45" },
+            { name: "Passing Period", start: "10:45", end: "10:55" },
+
+            { name: "2nd Period Final", start: "10:55", end: "12:55" },
+
+            { name: "Lunch", start: "12:55", end: "13:30" },
+            { name: "Passing Period", start: "13:30", end: "13:35" },
+
+            { name: "7th Period Final", start: "13:35", end: "15:35" }
+        ],
+
+        finalsDay2: [
+            { name: "3rd Period Final", start: "08:45", end: "10:45" },
+            { name: "Passing Period", start: "10:45", end: "11:05" },
+
+            { name: "4th Period Final", start: "11:05", end: "13:05" }
+        ],
+
+        finalsDay3: [
+            { name: "5th Period Final", start: "08:45", end: "10:45" },
+            { name: "Passing Period", start: "10:45", end: "11:05" },
+
+            { name: "6th Period Final", start: "11:05", end: "13:05" }
+        ]
+    },
+
+    exceptions: {}
+};
