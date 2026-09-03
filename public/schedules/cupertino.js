1b8976d1af44f1124ea7c49a19092227f504505a
xdinorun <162236514+xdinorun@users.noreply.github.com>
2026-09-02 19:36:25 -0700

    Create cupertino.js

---
+++ public/schedules/cupertino.js
@@ -0,0 +1,73 @@
+window.PearBellSchedules = window.PearBellSchedules || {};
+
+window.PearBellSchedules.cupertino = {
+    name: "Cupertino High School",
+    schoolYear: "2026-2027",
+
+    schedules: {
+        monday: [
+            { name: "1st Period", start: "08:30", end: "09:15" },
+            { name: "Passing Period", start: "09:15", end: "09:20" },
+
+            { name: "2nd Period", start: "09:20", end: "10:05" },
+            { name: "Passing Period", start: "10:05", end: "10:10" },
+
+            { name: "Tutorial", start: "10:10", end: "10:35" },
+            { name: "Passing Period", start: "10:35", end: "10:40" },
+
+            { name: "3rd Period", start: "10:40", end: "11:25" },
+
+            { name: "Brunch", start: "11:25", end: "11:40" },
+            { name: "Passing Period", start: "11:40", end: "11:50" },
+
+            { name: "4th Period", start: "11:50", end: "12:35" },
+            { name: "Passing Period", start: "12:35", end: "12:40" },
+
+            { name: "5th Period", start: "12:40", end: "13:25" },
+
+            { name: "Lunch", start: "13:25", end: "14:05" },
+            { name: "Passing Period", start: "14:05", end: "14:15" },
+
+            { name: "6th Period", start: "14:15", end: "15:00" },
+            { name: "Passing Period", start: "15:00", end: "15:05" },
+
+            { name: "7th Period", start: "15:05", end: "15:50" }
+        ],
+
+        tuesdayThursday: [
+            { name: "1st Period", start: "08:30", end: "10:00" },
+            { name: "Passing Period", start: "10:00", end: "10:05" },
+
+            { name: "2nd Period", start: "10:05", end: "11:35" },
+
+            { name: "Brunch", start: "11:35", end: "11:50" },
+            { name: "Passing Period", start: "11:50", end: "12:00" },
+
+            { name: "3rd Period", start: "12:00", end: "13:30" },
+
+            { name: "Lunch", start: "13:30", end: "14:10" },
+            { name: "Passing Period", start: "14:10", end: "14:20" },
+
+            { name: "7th Period", start: "14:20", end: "15:50" }
+        ],
+
+        wednesdayFriday: [
+            { name: "4th Period", start: "08:30", end: "10:05" },
+            { name: "Passing Period", start: "10:05", end: "10:10" },
+
+            { name: "Tutorial", start: "10:10", end: "10:50" },
+
+            { name: "Brunch", start: "10:50", end: "11:05" },
+            { name: "Passing Period", start: "11:05", end: "11:15" },
+
+            { name: "5th Period", start: "11:15", end: "12:45" },
+
+            { name: "Lunch", start: "12:45", end: "13:25" },
+            { name: "Passing Period", start: "13:25", end: "13:35" },
+
+            { name: "6th Period", start: "13:35", end: "15:05" }
+        ]
+    },
+
+    exceptions: {}
+};
